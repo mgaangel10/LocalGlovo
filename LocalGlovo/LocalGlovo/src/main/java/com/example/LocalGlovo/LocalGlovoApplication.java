dd3de5be32a012cdb1c37b311ef2b51086500999
mgaangel10 <mg.aangel10@gmail.com>
2024-02-20 08:34:51 +0100

    subida de proyecto

---
+++ LocalGlovo/LocalGlovo/src/main/java/com/example/LocalGlovo/LocalGlovoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LocalGlovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalGlovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalGlovoApplication.class, args);
+	}
+
+}
